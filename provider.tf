e4c4ea2577587191b4269e175fe673941ddc2910
Abhishek Guhathakurta <abhi21gt@gmail.com>
2024-12-11 15:10:30 +0000

    Updated provider and fetching credentials at runtime

--- provider.tf
+++ provider.tf
@@ -4,3 +4,22 @@ provider "azurerm" {
 }
 
 # Declare input variables for Azure authentication
+variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
