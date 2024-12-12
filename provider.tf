a10003553e6b015681f03b984a0e9d336a81af95
Abhishek Guhathakurta <abhi21gt@gmail.com>
2024-12-12 08:50:40 +0000

    Explicitly disable MSI to use the provided credentials

--- provider.tf
+++ provider.tf
@@ -5,6 +5,7 @@ provider "azurerm" {
   client_secret   = var.client_secret
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
+  use_msi         = false # Explicitly disable MSI to use the provided credentials
 }
 
 resource "azurerm_resource_group" "rg1" {
